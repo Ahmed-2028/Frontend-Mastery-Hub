76941d805fe14efc1552740b7d5524107fdf18e4
Ahmed - 2028 <mohamedmahmoued277@gmail.com>
2026-04-09 10:20:07 +0200

    Clean up comments in task1.js
    Removed unnecessary comment and link from task1.js

--- Mini-Project-01/Daily-Tasks/JAVASCRIPT/task1.js
+++ Mini-Project-01/Daily-Tasks/JAVASCRIPT/task1.js
@@ -13,8 +13,3 @@ console.log("Hallo");
 // Explanation: The first line will execute in the console, 
 // while the second line is ignored by the browser 
 // because it is marked as a comment using //.
-> [!TIP]
-> **Click here:** [⚡ Run & Test the Code Live](https://stackblitz.com/edit/js-bwbadudk?file=index.js&devtoolsheight=33)
-
-*Once the link opens, the output will automatically appear in the **Console** section at the bottom.*
----
