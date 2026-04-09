c3785fd566b4cb0ca8a0fcd33edaa2818277b88a
Ahmed - 2028 <mohamedmahmoued277@gmail.com>
2026-04-09 06:24:17 +0200

    Add introductory comments and console log in task1.js

--- Mini-Project-01/Daily-Tasks/JAVASCRIPT/task1.js
+++ Mini-Project-01/Daily-Tasks/JAVASCRIPT/task1.js
@@ -1,1 +1,15 @@
-// First Task
+// ==========================================
+// ☀️ TITLE: DAY 01
+// ==========================================
+
+// Sub-Title: Chapter 1 - Intro to Programming
+
+// 1. Using the console to print a message
+console.log("Hallo");
+
+// 2. Using comments to leave notes or disable code (Commented out)
+// console.log("Hallo"); 
+
+// Explanation: The first line will execute in the console, 
+// while the second line is ignored by the browser 
+// because it is marked as a comment using //.
