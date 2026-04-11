849be7320a082a6899fcfcfb453ed95a91a16c15
Ahmed - 2028 <mohamedmahmoued277@gmail.com>
2026-04-11 15:36:43 +0200

    Create task11.js

---
+++ Mini-Project-01/Daily-Tasks/JAVASCRIPT/task11.js
@@ -0,0 +1,13 @@
+// Create an array (list) containing three string elements
+let subjects = ["Logic", "Arabic", "Programming"];
+
+// Print the total number of items in the array (length)
+console.log(subjects.length); 
+// Access and print the first item using index 0
+console.log(subjects[0]); 
+
+// Loop through the entire array starting from index 0 to the last item
+for (let i = 0; i < subjects.length; i = i + 1) {
+    // Print each subject name one by one
+    console.log(subjects[i]);
+}
