a26d352419f909f412b40c032afb60ecf0a03b96
Ahmed - 2028 <mohamedmahmoued277@gmail.com>
2026-04-11 15:32:42 +0200

    Create task10.js

---
+++ Mini-Project-01/Daily-Tasks/JAVASCRIPT/task10.js
@@ -0,0 +1,9 @@
+// Store a number inside a string (text format)
+let myText = "1000";
+// Use the Number() function to convert the string "1000" into a real number 1000
+let myNumber = Number(myText);
+
+// Check and print the data type of the new variable (result: number)
+console.log(typeof myNumber); 
+// Use strict equality (===) to check if both value and type match the number 1000
+console.log(myNumber === 1000);
