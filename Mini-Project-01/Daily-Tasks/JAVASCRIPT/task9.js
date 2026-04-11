1c26c23110abb5a845447540ca3419608e7dd8c9
Ahmed - 2028 <mohamedmahmoued277@gmail.com>
2026-04-11 15:31:35 +0200

    Create task9.js

---
+++ Mini-Project-01/Daily-Tasks/JAVASCRIPT/task9.js
@@ -0,0 +1,11 @@
+// Define a custom function that takes two inputs (a and b) and returns their sum
+function sumNumbers(a, b) {
+    return a + b;
+}
+// Call the function with 10 and 20, then print the result (30)
+console.log(sumNumbers(10, 20)); 
+
+// Generate a random decimal between 0 and 1, multiply by 10
+let myRandom = Math.random() * 10;
+// Use Math.floor to remove the decimal part and get a whole number between 0 and 9
+console.log(Math.floor(myRandom));
