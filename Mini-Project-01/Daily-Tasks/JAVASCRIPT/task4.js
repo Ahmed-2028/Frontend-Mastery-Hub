42591509785d20e0697f211b1984279de42d8fae
Ahmed - 2028 <mohamedmahmoued277@gmail.com>
2026-04-11 04:32:31 +0200

    Create task4.js

---
+++ Mini-Project-01/Daily-Tasks/JAVASCRIPT/task4.js
@@ -0,0 +1,5 @@
+let isSubscribed = true; // Define a variable to check subscription status
+
+if (isSubscribed == true) { // Check if the user is subscribed
+    console.log("Welcome! You have access to the content."); // This runs only if the condition is true
+}
